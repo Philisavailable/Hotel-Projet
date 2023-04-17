113c03713ac838fd279ad05c009baf4fe85453c6
Philisavailable <bertola.philippe@gmail.com>
2023-04-17 11:27:18 +0200

    make entity

---
+++ src/Entity/Chambre.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChambreRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ChambreRepository::class)]
+class Chambre
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $descriptionCourte = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $descriptionLongue = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $photo = null;
+
+    #[ORM\Column(length: 4)]
+    private ?string $prixJournalier = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescriptionCourte(): ?string
+    {
+        return $this->descriptionCourte;
+    }
+
+    public function setDescriptionCourte(string $descriptionCourte): self
+    {
+        $this->descriptionCourte = $descriptionCourte;
+
+        return $this;
+    }
+
+    public function getDescriptionLongue(): ?string
+    {
+        return $this->descriptionLongue;
+    }
+
+    public function setDescriptionLongue(string $descriptionLongue): self
+    {
+        $this->descriptionLongue = $descriptionLongue;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getPrixJournalier(): ?string
+    {
+        return $this->prixJournalier;
+    }
+
+    public function setPrixJournalier(string $prixJournalier): self
+    {
+        $this->prixJournalier = $prixJournalier;
+
+        return $this;
+    }
+}
