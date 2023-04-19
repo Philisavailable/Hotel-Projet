572f8295509f1a2145065f6e284db96b780d75bc
wafafoufa88 <126761425+wafafoufa88@users.noreply.github.com>
2023-04-19 12:15:49 +0200

    partie spa-wafa

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,4 +13,35 @@ class DefaultController extends AbstractController
     {
         return $this->render('default/show_home.html.twig');
     }
+    //*********************spa************** */
+    #[Route('/spa/voir-spa', name: 'show_spa', methods: ['GET'])]
+    public function showSpa(): Response
+    {
+        
+        return $this->render('/spa/show_spa.html.twig');
+    }
+
+    //*****************spa-détente***** */
+    #[Route('/spa/voir-spa-detente', name: 'show_spa-detente', methods: ['GET'])]
+    public function showSpaDetente(): Response
+    {
+        
+        return $this->render('/spa/spa-detente.html.twig');
+    }
+    //*****************spa-relaxante************ */
+    #[Route('/spa/voir-spa-relaxant', name: 'show_spa-relaxant', methods: ['GET'])]
+    public function showSpaRelaxant(): Response
+    {
+        
+        return $this->render('/spa/spa_relaxant.html.twig');
+    }
+
+    //******************spa-palaisir********** */
+
+    #[Route('/spa/voir-spa-plaisir', name: 'show_spa-plaisir', methods: ['GET'])]
+    public function showSpaPlaisir(): Response
+    {
+        
+        return $this->render('spa/spa_plaisir.html.twig');
+    }
 }
